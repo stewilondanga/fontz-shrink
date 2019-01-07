2bd302c375f5eb698d15b6a82b679441120daa6b
Stewart ilondanga <stilondanga@gmail.com>
2019-01-07 19:32:20 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var navigate = (function() {
     $('.' + dataName).toggle();
   });
 })();
-/*
+
 document.querySelector("input").addEventListener("input", function() {
   let newFontSize = (48 - this.value.length);
   if (newFontSize > 30) newFontSize = 30;
